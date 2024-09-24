4eeedc307a733bc5f7079338be8a3cb970c6d880
Johannes <jluetzenho@stud.hs-heilbronn.de>
2024-09-24 10:37:08 +0200

    feat: add Button Text

--- src/js/magneticIcons.js
+++ src/js/magneticIcons.js
@@ -11,30 +11,34 @@ export function animateMagneticSmiley() {
     return {
       background: $event.target.querySelector(".magnetic-background"),
       element: $event.target.querySelector(".magnetic-element"),
+      text: $event.target.querySelector(".magnetic-text"),
     };
   }
 
   function move($event) {
     const x = $event.layerX - $event.target.clientWidth / 2;
     const y = $event.layerY - $event.target.clientHeight / 2;
-    const { background, element } = getChilds($event);
+    const { background, element, text } = getChilds($event);
 
     background.style.transform = `translate(${x / 6}px, ${y / 6}px)`;
     element.style.transform = `translate(${x / 4}px, ${y / 4}px)`;
+    text.style.transform = `translate(${x / 4}px, ${y / 4}px)`;
   }
 
   function startAnimation($event) {
-    const { background, element } = getChilds($event);
+    const { background, element, text } = getChilds($event);
     const transition = `all ${time}ms ease`;
     background.style.transition = transition;
     element.style.transition = transition;
+    text.style.transition = transition;
   }
 
   function endAnimation($event) {
-    const { background, element } = getChilds($event);
+    const { background, element, text } = getChilds($event);
     setTimeout(() => {
       background.style.transition = "";
       element.style.transition = "";
+      text.style.transition = "";
     }, time);
   }
 
@@ -44,12 +48,13 @@ export function animateMagneticSmiley() {
   }
 
   function end($event) {
-    const { background, element } = getChilds($event);
+    const { background, element, text } = getChilds($event);
 
     startAnimation($event);
 
     background.style.transform = `translate(0px, 0px)`;
     element.style.transform = `translate(0px, 0px)`;
+    text.style.transform = `translate(0px, 0px)`;
 
     endAnimation($event);
   }
--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,5 @@ import { animateMagneticSmiley } from "./magneticIcons";
 animateMagneticSmiley();
 fadeInElem();
 
+
+
